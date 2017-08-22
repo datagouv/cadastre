f6ab187723301ff3f9bfc9e207f36e7e6fceac7a
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2017-08-22 16:52:30 +0200

    Prepare CLI for parallelization

--- bin/cli.js
+++ bin/cli.js
@@ -1,18 +1,29 @@
 #!/usr/bin/env node
-const path = require('path')
+const { resolve } = require('path')
+const { promisify } = require('util')
+
 const program = require('commander')
 const ProgressBar = require('ascii-progress')
+const mkdirp = require('mkdirp')
+
 const { version } = require('../package.json')
 const FileWriter = require('../lib/output/files')
 const { extractFromBundle } = require('../lib/extract')
 
+const mkdirpAsync = promisify(mkdirp)
+
 program
   .version(version)
-  .arguments('<depCode> <srcBundle> <outDest>')
-  .action((depCode, srcBundle, outDest) => {
-    const writer = new FileWriter(path.resolve(process.cwd(), outDest))
+  .arguments('<depCode> <src> <dest>')
+  .action(async (depCode, src, dest) => {
+    const depSrc = resolve(process.cwd(), src, `./dep${depCode}.zip`)
+    const depDest = resolve(process.cwd(), dest, `./dep${depCode}`)
 
-    const converter = extractFromBundle(path.resolve(process.cwd(), srcBundle), writer, depCode)
+    // Prepare output
+    await mkdirpAsync(depDest)
+    const writer = new FileWriter(depDest)
+
+    const converter = extractFromBundle(depSrc, writer, depCode)
     let bar
 
     converter
@@ -20,11 +31,12 @@ program
       .on('error', console.error)
       .on('start', () => {
         bar = new ProgressBar({
-          schema: '  converting [:bar] :percent :etas',
+          schema: '  converting :depCode [:bar] :percent :etas',
           total: converter.total,
         })
+        bar.update(0, { depCode })
       })
-      .on('file:converted', () => bar.tick())
-      .on('file:ignored', () => bar.tick())
+      .on('file:converted', () => bar.tick(1, { depCode }))
+      .on('file:ignored', () => bar.tick(1, { depCode }))
   })
   .parse(process.argv)
