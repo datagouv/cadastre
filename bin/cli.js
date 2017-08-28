672018fc4ea0bb1e7febf1c41f3df4cfb9afb1c0
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2017-08-28 10:54:42 +0200

    cli: add decompress command

--- bin/cli.js
+++ bin/cli.js
@@ -5,44 +5,16 @@ const program = require('commander')
 
 const { version } = require('../package.json')
 
-
 const numCPUs = cpus().length
 
 program
   .version(version)
 
-    const departementsFound = files
-      .map(f => {
-        const res = f.match(/^dep([0-9A-Z]{2,3}).zip$/)
-        if (res) return res[1]
-        return res ? res[1] : null
-      })
-      .filter(res => Boolean(res))
-
-    const queued = [...departementsFound]
-    const finished = []
-
-    const workers = []
-
-    function eventuallyFinish() {
-      if (finished.length === departementsFound.length) {
-        console.log('Finished')
-        process.exit(0)
-      }
-    }
-
-    for (let i = 0; i < numWorkers; i++) {
-      const worker = createWorker(srcDir, destDir, writeRaw, Worker)
-      workers.push(worker)
-      worker.on('end', ({ codeDep }) => {
-        finished.push(codeDep)
-      })
-      worker.on('ready', () => {
-        if (queued.length === 0) return eventuallyFinish()
-        const codeDep = queued.shift()
-        worker.extractDepartement(codeDep)
-      })
-    }
+program
+  .command('decompress <srcDir> <destDir>')
+  .description('decompress DGFiP departements bundles')
+  .action((srcDir, destDir) => {
+    require('../lib/commands/decompress')(srcDir, destDir)
   })
 
 program
