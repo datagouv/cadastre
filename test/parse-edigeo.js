ccf8dd844506b6edb0c79b4dce39d94f6cbfd4fb
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2017-10-31 15:28:02 +0100

    Add parseLine test

---
+++ test/parse-edigeo.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const test = require('ava')
+const { parseLine } = require('../lib/parse/raw')
+
+test('parseLine-COR', t => {
+  const line = 'CORCC23:+579588.05;+6680876.27;'
+  t.deepEqual(parseLine(line).parsedValue, [579588.05, 6680876.27])
+})
