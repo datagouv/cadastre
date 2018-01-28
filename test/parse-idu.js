a502377efefc91dab64ce94c80a87ac496ee4217
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2018-01-28 10:31:12 +0100

    Add commune, prefixe, section and numero(feuilles) to models
    Fix #11

--- test/parse-idu.js
+++ test/parse-idu.js
@@ -4,11 +4,11 @@ const test = require('ava')
 const {parseParcelleIDU, parseSectionIDU} = require('../lib/convert/pci')
 
 test('parseParcelleIDU', t => {
-  t.deepEqual(parseParcelleIDU('123000AC0001'), {com: '123', prefix: '000', section: 'AC', parcelle: '0001'})
+  t.deepEqual(parseParcelleIDU('123000AC0001'), {com: '123', prefixe: '000', section: 'AC', parcelle: '0001'})
   t.throws(() => parseParcelleIDU('123000AC001')) // Too small
 })
 
 test('parseSectionIDU', t => {
-  t.deepEqual(parseSectionIDU('123000AC'), {com: '123', prefix: '000', section: 'AC'})
+  t.deepEqual(parseSectionIDU('123000AC'), {com: '123', prefixe: '000', section: 'AC'})
   t.throws(() => parseParcelleIDU('123000AC001')) // Too long
 })
