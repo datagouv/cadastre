ce5afa0522de88c1d56fa05178ee3ebcb79f53a0
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2018-05-16 23:42:07 +0200

    Fix tests

--- test/prepare-models.js
+++ test/prepare-models.js
@@ -5,21 +5,21 @@ const {prepareSection, prepareParcelle} = require('../lib/convert/pci')
 
 test('prepareSection', t => {
   t.is(prepareSection({
-    feuille: '01123000AC01', geometry: {}, properties: {IDU: '123000AC'}
-  }).properties.code, 'AC')
+    geometry: {}, properties: {IDU: '123000AC'}
+  }, '01123000AC01').properties.code, 'AC')
   t.is(prepareSection({
-    feuille: '011230000Z01', geometry: {}, properties: {IDU: '1230000Z'}
-  }).properties.code, 'Z')
+    geometry: {}, properties: {IDU: '1230000Z'}
+  }, '011230000Z01').properties.code, 'Z')
 })
 
 test('prepareParcelle', t => {
   t.is(prepareParcelle({
-    feuille: '01123000AC01', geometry: {}, properties: {IDU: '123000AC0001'}
-  }).properties.numero, '1')
+    geometry: {}, properties: {IDU: '123000AC0001'}
+  }, '01123000AC01').properties.numero, '1')
   t.is(prepareParcelle({
-    feuille: '011230000Z01', geometry: {}, properties: {IDU: '1230000Z0650'}
-  }).properties.numero, '650')
+    geometry: {}, properties: {IDU: '1230000Z0650'}
+  }, '011230000Z01').properties.numero, '650')
   t.throws(() => prepareParcelle({
-    feuille: '01123000AC01', geometry: {}, properties: {IDU: null}
-  }))
+    geometry: {}, properties: {IDU: null}
+  }, '01123000AC01'))
 })
